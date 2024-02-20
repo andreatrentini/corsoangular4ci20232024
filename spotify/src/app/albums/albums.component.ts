55964730a487d5a0e16bd042887c0ab10f445eb2
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2024-02-20 11:50:01 +0000

    Subscribe a params URL di albums

--- spotify/src/app/albums/albums.component.ts
+++ spotify/src/app/albums/albums.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.css'
 })
-export class AlbumsComponent implements OnInit {
+export class AlbumsComponent implements OnInit, OnDestroy {
 
+  artistsId!: string;
+
+  // L'oggetto di tipo subscription (abbonamento) mi consente di gestire il subscribe su un observable
+  // E' importante eseguire sempre l'unsubscribe prima di uscire dal componente
+  routeSub!: Subscription;
+
+  // ActivatedRoute mi consente di ottenere informazioni dalla URL corrente
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    console.log(this.activatedRoute.snapshot)
+    // Params è un observable che mi consente di controllare se la URL (in particolare il
+    // parametro idArtista) cambia.
+    this.routeSub = this.activatedRoute.params.subscribe(params => {
+      this.artistsId = params['id'];
+      console.log(this.artistsId);
+    })
+  }
+
+  // Importante: eseguire unsubscribe!
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
 
 }
